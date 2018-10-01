a7d688d5b15a6ff8a946e673b0ab6f8ac5ccce2c
Mauricio Rodriguez <mrodriguezlara@qualogy.com>
2018-10-01 13:09:45 +0200

    Firs commit

---
+++ Test_Twitter_Analysis.py
@@ -0,0 +1,20 @@
+import unittest
+import nltk
+from nltk.corpus import stopwords
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_stop_words_content(self):
+        stopwords_set = set(stopwords.words("english"))
+        self.assertIn("against",stopwords_set,"Stop words content passed")
+       # self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_stop_words_size(self):
+        stopwords_set = set(stopwords.words("english"))
+        print(len(stopwords_set))
+        self.assertEqual(179,len(stopwords_set),"Stop words size passed")
+       # self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
