4f7dbaf1ed8677ee5c7446916ab93bf7855ec0ba
Mauricio Rodriguez <mrodriguezlara@qualogy.com>
2018-10-01 18:25:42 +0200

    Test files

--- tests/Test_Twitter_Analysis.py
+++ tests/Test_Twitter_Analysis.py
@@ -1,26 +1,27 @@
-
 import unittest
 import xmlrunner
 from nltk.corpus import stopwords
+import nltk
+
 
 class TestStringMethods(unittest.TestCase):
 
     def test_stop_words_content(self):
         stopwords_set = set(stopwords.words("english"))
-        self.assertIn("against",stopwords_set,"Stop words content passed")
-       # self.assertEqual('foo'.upper(), 'FOO')
+        self.assertIn("against", stopwords_set, "Stop words content passed")
+
+    # self.assertEqual('foo'.upper(), 'FOO')
 
     def test_stop_words_size(self):
         stopwords_set = set(stopwords.words("english"))
         print(len(stopwords_set))
-        self.assertEqual(179,len(stopwords_set),"Stop words size passed")
-       # self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual(179, len(stopwords_set), "Stop words size passed")
+    # self.assertEqual('foo'.upper(), 'FOO')
+
 
 if __name__ == '__main__':
-
-
-    unittest.main(
-        testRunner=xmlrunner.XMLTestRunner(output='test-reports.xml'),
-        failfast=False,
-        buffer=False,
-        catchbreak=False)
+    nltk.download('stopwords')
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'
+                                                     ), failfast=False,
+                  buffer=False,
+                  catchbreak=False)
